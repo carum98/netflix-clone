39fdda762438212a6ef919b2d528b8428caf6f6c
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-02-05 00:42:49 -0600

    Add header

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,15 @@ const { data } = await useFetch('/api/movies')
 </script>
 
 <template>
+	<TheHeader />
+	<h2 class="subtitle">Trending Now</h2>
+	<MovieCarousel :movies="data.data" />
+	<h2 class="subtitle">Family Movies</h2>
+	<MovieCarousel :movies="data.data" />
+	<h2 class="subtitle">Comedy Movies</h2>
+	<MovieCarousel :movies="data.data" />
+	<h2 class="subtitle">Horror Movies</h2>
+	<MovieCarousel :movies="data.data" />
+	<h2 class="subtitle">Documentaries</h2>
 	<MovieCarousel :movies="data.data" />
 </template>
