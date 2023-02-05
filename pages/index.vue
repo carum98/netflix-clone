9c5ee4c4c126ad8c1dbd29234ca93c5c3c12a9e7
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-02-05 01:06:20 -0600

    Multiple categories

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,86 @@
 <script setup>
-const { data } = await useFetch('/api/movies')
+const [trending, originals, topRated, action, family, comedy, horror, documentaries, romance, mystery, scifi, western, animation, tv] = await Promise.all([
+	useFetch('/api/movies?q=trending'),
+	useFetch('/api/movies?q=originals'),
+	useFetch('/api/movies?q=top_rated'),
+	useFetch('/api/movies?q=action'),
+	useFetch('/api/movies?q=family'),
+	useFetch('/api/movies?q=comedy'),
+	useFetch('/api/movies?q=horror'),
+	useFetch('/api/movies?q=documentaries'),
+	useFetch('/api/movies?q=romance'),
+	useFetch('/api/movies?q=mystery'),
+	useFetch('/api/movies?q=scifi'),
+	useFetch('/api/movies?q=western'),
+	useFetch('/api/movies?q=animation'),
+	useFetch('/api/movies?q=tv')
+])
+
+const data = {
+	trending: {
+		title: 'Trending Now',
+		movies: trending.data,
+	},
+	originals: {
+		title: 'Netflix Originals',
+		movies: originals.data,
+	},
+	topRated: {
+		title: 'Top Rated',
+		movies: topRated.data,
+	},
+	action: {
+		title: 'Action',
+		movies: action.data,
+	},
+	family: {
+		title: 'Family',
+		movies: family.data,
+	},
+	comedy: {
+		title: 'Comedy',
+		movies: comedy.data,
+	},
+	horror: {
+		title: 'Horror',
+		movies: horror.data,
+	},
+	documentaries: {
+		title: 'Documentaries',
+		movies: documentaries.data,
+	},
+	romance: {
+		title: 'Romance',
+		movies: romance.data,
+	},
+	mystery: {
+		title: 'Mystery',
+		movies: mystery.data,
+	},
+	scifi: {
+		title: 'Sci-Fi',
+		movies: scifi.data,
+	},
+	western: {
+		title: 'Western',
+		movies: western.data,
+	},
+	animation: {
+		title: 'Animation',
+		movies: animation.data,
+	},
+	tv: {
+		title: 'TV',
+		movies: tv.data,
+	}
+}
 </script>
 
 <template>
 	<TheHeader />
-	<h2 class="subtitle">Trending Now</h2>
-	<MovieCarousel :movies="data.data" />
-	<h2 class="subtitle">Family Movies</h2>
-	<MovieCarousel :movies="data.data" />
-	<h2 class="subtitle">Comedy Movies</h2>
-	<MovieCarousel :movies="data.data" />
-	<h2 class="subtitle">Horror Movies</h2>
-	<MovieCarousel :movies="data.data" />
-	<h2 class="subtitle">Documentaries</h2>
-	<MovieCarousel :movies="data.data" />
+
+	<template v-for="item in data">
+		<h2 class="subtitle">{{ item.title }}</h2>
+		<MovieCarousel :movies="item.movies.value.data" />
+	</template>
 </template>
--- server/api/movies/index.ts
+++ server/api/movies/index.ts
@@ -4,7 +4,50 @@ import { httpClient } from "../../lib/httpClient"
 export default defineEventHandler(async (event) => {
 	const client = httpClient()
 
-	const response = await client.get('/trending/all/week')
+	const { q } = getQuery(event)
+
+	let path = ''
+
+	switch (q) {
+		case 'originals':
+			path = '/discover/tv?with_networks=213'
+			break;
+		case 'top_rated':
+			path = '/movie/top_rated'
+			break;
+		case 'action':
+			path = '/discover/movie?with_genres=28'
+			break;
+		case 'comedy':
+			path = '/discover/movie?with_genres=35'
+			break;
+		case 'horror':
+			path = '/discover/movie?with_genres=27'
+			break;
+		case 'romance':
+			path = '/discover/movie?with_genres=10749'
+			break;
+		case 'mystery':
+			path = '/discover/movie?with_genres=9648'
+			break;
+		case 'sci_fi':
+			path = '/discover/movie?with_genres=878'
+			break;
+		case 'western':
+			path = '/discover/movie?with_genres=37'
+			break;
+		case 'animation':
+			path = '/discover/movie?with_genres=16'
+			break;
+		case 'tv':
+			path = '/discover/movie?with_genres=10770'
+			break;
+		default:
+			path = '/trending/all/week'
+			break;
+	}
+
+	const response = await client.get(path)
 
 	return {
 		data: response.results.map((movie: Object) => new Movie(movie))
